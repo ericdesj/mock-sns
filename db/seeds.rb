c52f99a4632ab95a75625f1b4ff147a49f52d281
Eric Desjardins <eric.desjardins11@gmail.com>
2017-03-03 13:55:45 -0400

    Updated seeders with user emails

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 500.times do |n|
   first_name  = Faker::Name.first_name
   last_name  = Faker::Name.last_name
+  user_email = Faker::Internet.email
   birthday = Faker::Date.between(70.years.ago, 13.years.ago)
   university = Faker::University.name
   work = Faker::Company.name
@@ -16,30 +17,20 @@
   email = "example-#{n+1}@mocksns.com"
   User.create!(first_name:  first_name,
                last_name: last_name,
+               user_email: user_email,
                birthday: birthday,
                profile_picture: "https://randomuser.me/api/portraits/women/99.jpg",
                education: university,
                work: work)
 end
 
-# 1000.times do |n|
-#   Friendship.create!(
-#       user_id: rand(1..1000),
-#       friend_id: rand(1..1000)
-#   )
-# end
-
 for user in User.all
-
   10.times do |n|
-
     friend_id = rand(1..500)
-
     loop do
       friend_id = rand(1..500)
       break if friend_id != user.id
     end
-
     Friendship.create!(
         user_id: user.id,
         friend_id: friend_id
