ad22ff152d5b475920fb566c057973b0fbb9a5c4
Eric Desjardins <eric.desjardins11@gmail.com>
2017-03-03 14:15:20 -0400

    Added gender to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303173233) do
+ActiveRecord::Schema.define(version: 20170303180444) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer  "user_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170303173233) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "user_email"
+    t.string   "gender"
   end
 
 end
